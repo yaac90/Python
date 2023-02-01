504fb3c181946a42dfc5816ef696afd10a9b7541
yaac90 <yaac9@hotmail.com>
2023-01-31 20:09:51 -0500

    second day

---
+++ day_one.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+""" first day, i will practice python basic sintax """
+
+Dictionary_example = { 'name': 'yuber', 'age': 32, 'married': False}
+# Printing the type of the variable Dictionary_example
+print(type(Dictionary_example))
+
+# A set, it is a collection which is unordered and unindexed. No duplicate members.
+set_example = { 'Uruguay', 'Colombia', 'Brasil', 'USA'}
+print(type(set_example))
+
+list_example = [ 'Peru', 23, 'Argentina', 10, 'Canada']
+# Printing the type of the variable list_example
+print(type(list_example))
+
+tuple_example = ( 'Barranquilla', 'Medellin', 'San Francisco', ' Berlin')
+# Printing the type of the variable tuple_example
+print(type(tuple_example))
+
