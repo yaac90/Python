3be5f0fe1409149c26c9984b8ef6657d870d144e
yaac90 <yaac9@hotmail.com>
2023-02-14 16:30:14 -0500

    function by to checke if the word is palindrome

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def palindrome(string_Word):
+    """
+    It checks if a word is a palindrome.
+    
+    :param string_Word: The word that will be checked to see if it's a palindrome
+    """
+    if len(string_word >= 0)
+        lower_string = string_Word.lower()
+        return  lower_string == lower_string[::-1]
+    else
+        return "String must to be bigger than 0"
